8f89423b5a77dfed13014d9046c212e128efd68f
Vijaya Lingagiri <vlingagiri@gmail.com>
2024-10-27 10:23:47 +0000

    refactor

---
+++ app/economic_indicators.py
@@ -0,0 +1,9 @@
+import requests
+import json
+import os
+from app.config import ALPHA_VANTAGE_API_KEY
+
+# from src.config import ALPHA_VANTAGE_API_KEY
+# from src.logger import logger
+
+# CACHE_FILE = 'data/economic_indicators_cache.json'
